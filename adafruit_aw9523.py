8117a5291a1e32ba372a1e51f7d1b8ba92268468
lady ada <limor@ladyada.net>
2021-01-19 21:25:53 -0500

    write outputs

--- adafruit_aw9523.py
+++ adafruit_aw9523.py
@@ -1,4 +1,3 @@
-# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
 # SPDX-FileCopyrightText: Copyright (c) 2021 ladyada for Adafruit
 #
 # SPDX-License-Identifier: MIT
@@ -26,7 +25,81 @@ Implementation Notes
 * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
 """
 
-# imports
+import time
+import struct
+import adafruit_bus_device.i2c_device as i2c_device
+from adafruit_register.i2c_struct import ROUnaryStruct, UnaryStruct
+from adafruit_register.i2c_bit import RWBit
+from micropython import const
 
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_AW9523.git"
+
+_AW9523_DEFAULT_ADDR = const(0x58)
+_AW9523_REG_CHIPID = const(0x10)     # Register for hardcode chip ID
+_AW9523_REG_SOFTRESET = const(0x7F)  # Register for soft resetting
+_AW9523_REG_INPUT0 = const(0x00)     # Register for reading input values
+_AW9523_REG_OUTPUT0 = const(0x02)    # Register for writing output values
+_AW9523_REG_CONFIG0 = const(0x04)    # Register for configuring direction
+_AW9523_REG_INTENABLE0 = const(0x06) # Register for enabling interrupt
+_AW9523_REG_GCR = const(0x11)        # Register for general configuration
+_AW9523_REG_LEDMODE = const(0x12)    # Register for configuring const current
+
+
+class AW9523:
+    
+    _chip_id = ROUnaryStruct(_AW9523_REG_CHIPID, "<B")
+    _reset_reg = UnaryStruct(_AW9523_REG_SOFTRESET, "<B")
+
+    # Set all 16 gpio outputs
+    outputs = UnaryStruct(_AW9523_REG_OUTPUT0, "<H")
+    # Read all 16 gpio inputs
+    inputs = UnaryStruct(_AW9523_REG_INPUT0, "<H")
+    # Set all 16 gpio interrupt enable
+    _interrupt_enables = UnaryStruct(_AW9523_REG_CONFIG0, "<H")
+    # Set all 16 gpio directions
+    _directions = UnaryStruct(_AW9523_REG_CONFIG0, "<H")
+    # Set all 16 gpio LED modes
+    _LED_modes = UnaryStruct(_AW9523_REG_LEDMODE, "<H")
+
+    # Whether port 0 is push-pull
+    port0_push_pull = RWBit(_AW9523_REG_GCR, 4)
+    
+
+    def __init__(self, i2c_bus, address=_AW9523_DEFAULT_ADDR, reset=True):
+        self.i2c_device = i2c_device.I2CDevice(i2c_bus, address)
+
+        if self._chip_id != 0x23:
+            raise AttributeError("Cannot find a AW9523")
+        if reset:
+            self.reset()
+            self.port0_push_pull = True # pushpull output
+            self.directions = 0x0000 # all inputs!
+
+    def reset(self):
+        self._reset_reg = 0
+        
+
+    @property
+    def interrupt_enables(self):
+        return ~self._interrupt_enables & 0xFFFF
+
+    @interrupt_enables.setter
+    def interrupt_enables(self, enables):
+       self._interrupt_enables = ~enables & 0xFFFF
+
+    @property
+    def directions(self):
+        return ~self._directions & 0xFFFF
+
+    @directions.setter
+    def directions(self, dirs):
+       self._directions = ~dirs & 0xFFFF
+
+    @property
+    def LED_modes(self):
+        return ~self._LED_modes & 0xFFFF
+
+    @LED_modes.setter
+    def LED_modes(self, modes):
+       self._LED_modes = ~modes & 0xFFFF
